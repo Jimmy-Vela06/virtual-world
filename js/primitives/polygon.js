e4bb20337f92fa2efcf86b6ebccad84fa0e9e85e
Jimmy Vela <jimmyvela06@.com>
2024-04-05 03:28:17 -0500

    building polygons along the roads

--- js/primitives/polygon.js
+++ js/primitives/polygon.js
@@ -1,100 +1,128 @@
 class Polygon {
-  constructor(points) {
-    this.points = points;
+	constructor(points) {
+		this.points = points;
 
-    this.segments = [];
-    for (let i = 1; i <= points.length; i++) {
-      this.segments.push(new Segment(points[i - 1], points[i % points.length]));
-    }
-  }
-  static union(polys) {
-    Polygon.multiBreak(polys);
-    const keptSegments = [];
-    for (let i = 0; i < polys.length; i++) {
-      for (const seg of polys[i].segments) {
-        let keep = true;
-        for (let j = 0; j < polys.length; j++) {
-          if (i != j) {
-            if (polys[j].containsSegment(seg)) {
-              keep = false;
-              break;
-            }
-          }
-        }
-        if (keep) {
-          keptSegments.push(seg);
-        }
-      }
-    }
-    return keptSegments;
-  }
+		this.segments = [];
+		for (let i = 1; i <= points.length; i++) {
+			this.segments.push(new Segment(points[i - 1], points[i % points.length]));
+		}
+	}
+	static union(polys) {
+		Polygon.multiBreak(polys);
+		const keptSegments = [];
+		for (let i = 0; i < polys.length; i++) {
+			for (const seg of polys[i].segments) {
+				let keep = true;
+				for (let j = 0; j < polys.length; j++) {
+					if (i != j) {
+						if (polys[j].containsSegment(seg)) {
+							keep = false;
+							break;
+						}
+					}
+				}
+				if (keep) {
+					keptSegments.push(seg);
+				}
+			}
+		}
+		return keptSegments;
+	}
 
-  static multiBreak(polys) {
-    for (let i = 0; i <= polys.length; i++) {
-      for (let j = i + 1; j < polys.length; j++) {
-        Polygon.break(polys[i], polys[j]);
-      }
-    }
-  }
+	static multiBreak(polys) {
+		for (let i = 0; i <= polys.length; i++) {
+			for (let j = i + 1; j < polys.length; j++) {
+				Polygon.break(polys[i], polys[j]);
+			}
+		}
+	}
 
-  static break(polyOne, polyTwo) {
-    const segsOne = polyOne.segments;
-    const segsTwo = polyTwo.segments;
+	static break(polyOne, polyTwo) {
+		const segsOne = polyOne.segments;
+		const segsTwo = polyTwo.segments;
 
-    for (let i = 0; i < segsOne.length; i++) {
-      for (let j = 0; j < segsTwo.length; j++) {
-        const intersection = getIntersection(segsOne[i].p1, segsOne[i].p2, segsTwo[j].p1, segsTwo[j].p2);
+		for (let i = 0; i < segsOne.length; i++) {
+			for (let j = 0; j < segsTwo.length; j++) {
+				const intersection = getIntersection(
+					segsOne[i].p1,
+					segsOne[i].p2,
+					segsTwo[j].p1,
+					segsTwo[j].p2
+				);
 
-        if (intersection && intersection.offset != 1 && intersection.offset != 0) {
-          const point = new Point(intersection.x, intersection.y);
+				if (
+					intersection &&
+					intersection.offset != 1 &&
+					intersection.offset != 0
+				) {
+					const point = new Point(intersection.x, intersection.y);
 
-          let aux = segsOne[i].p2;
-          segsOne[i].p2 = point;
-          segsOne.splice(i + 1, 0, new Segment(point, aux));
-          aux = segsTwo[j].p2;
-          segsTwo[j].p2 = point;
-          segsTwo.splice(j + 1, 0, new Segment(point, aux));
-        }
-      }
-    }
-  }
+					let aux = segsOne[i].p2;
+					segsOne[i].p2 = point;
+					segsOne.splice(i + 1, 0, new Segment(point, aux));
+					aux = segsTwo[j].p2;
+					segsTwo[j].p2 = point;
+					segsTwo.splice(j + 1, 0, new Segment(point, aux));
+				}
+			}
+		}
+	}
 
-  containsSegment(seg) {
-    const midpoint = average(seg.p1, seg.p2);
-    return this.containsPoint(midpoint);
-  }
+	distanceToPoint(point) {
+		return Math.min(...this.segments.map((s) => s.distanceToPoint(point)));
+	}
 
-  containsPoint(point) {
-    const outerPoint = new Point(-1000, -1000);
-    let intersectionCount = 0;
-    for (const seg of this.segments) {
-      const int = getIntersection(outerPoint, point, seg.p1, seg.p2);
-      if (int) {
-        intersectionCount++;
-      }
-    }
-    return intersectionCount % 2 == 1;
-  }
+	distanceToPoly(poly) {
+		return Math.min(...this.points.map((p) => poly.distanceToPoint(p)));
+	}
 
-  drawSegments(ctx) {
-    for (const segment of this.segments) {
-      segment.draw(ctx, { color: getRandomColor(), width: 5 });
-    }
-  }
+	intersectsPoly(poly) {
+		for (let s1 of this.segments) {
+			for (let s2 of poly.segments) {
+				if (getIntersection(s1.p1, s1.p2, s2.p1, s2.p2)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 
-  draw(ctx, { stroke = 'blue', lineWidth = 3, fill = 'rgba(0,0,255,0.2)' } = {}) {
-    ctx.beginPath();
+	containsSegment(seg) {
+		const midpoint = average(seg.p1, seg.p2);
+		return this.containsPoint(midpoint);
+	}
 
-    ctx.strokeStyle = stroke;
-    ctx.fillStyle = fill;
-    ctx.lineWidth = lineWidth;
+	containsPoint(point) {
+		const outerPoint = new Point(-1000, -1000);
+		let intersectionCount = 0;
+		for (const seg of this.segments) {
+			const int = getIntersection(outerPoint, point, seg.p1, seg.p2);
+			if (int) {
+				intersectionCount++;
+			}
+		}
+		return intersectionCount % 2 == 1;
+	}
 
-    ctx.moveTo(this.points[0].x, this.points[0].y);
-    this.points.forEach((point) => {
-      ctx.lineTo(point.x, point.y);
-    });
-    ctx.closePath();
-    ctx.fill();
-    ctx.stroke();
-  }
+	drawSegments(ctx) {
+		for (const segment of this.segments) {
+			segment.draw(ctx, {color: getRandomColor(), width: 5});
+		}
+	}
+
+	draw(ctx, {stroke = 'blue', lineWidth = 3, fill = 'rgba(0,0,255,0.2)'} = {}) {
+		ctx.beginPath();
+
+		ctx.strokeStyle = stroke;
+		ctx.fillStyle = fill;
+		ctx.lineWidth = lineWidth;
+
+		ctx.moveTo(this.points[0].x, this.points[0].y);
+		this.points.forEach((point) => {
+			ctx.lineTo(point.x, point.y);
+		});
+		ctx.closePath();
+		ctx.fill();
+		ctx.stroke();
+	}
 }
--- js/world.js
+++ js/world.js
@@ -1,30 +1,123 @@
 class World {
-  constructor(graph, roadWidth = 100, roadRoundness = 9) {
-    this.graph = graph;
-    this.roadWidth = roadWidth;
-    this.roadRoundness = roadRoundness;
+	constructor(
+		graph,
+		roadWidth = 80,
+		roadRoundness = 20,
+		buildingWidth = 150,
+		buildingMinLength = 150,
+		spacing = 50
+	) {
+		this.graph = graph;
+		this.roadWidth = roadWidth;
+		this.roadRoundness = roadRoundness;
 
-    this.envelopes = [];
-    this.roadBorders = [];
+		this.buildingWidth = buildingWidth;
+		this.buildingMinLength = buildingMinLength;
+		this.spacing = spacing;
 
-    this.generate();
-  }
+		this.envelopes = [];
+		this.roadBorders = [];
 
-  generate() {
-    this.envelopes.length = 0;
+		this.buildings = [];
 
-    this.graph.segments.forEach((segment) => {
-      this.envelopes.push(new Envelope(segment, this.roadWidth, this.roadRoundness));
-    });
+		this.generate();
+	}
 
-    this.roadBorders = Polygon.union(this.envelopes.map((envelope) => envelope.poly));
-  }
+	generate() {
+		this.envelopes.length = 0;
 
-  draw(ctx) {
-    this.envelopes.forEach((envelope) => envelope.draw(ctx, { fill: '#BBB', stroke: '#BBB', lineWidth: 15 }));
+		this.graph.segments.forEach((segment) => {
+			this.envelopes.push(
+				new Envelope(segment, this.roadWidth, this.roadRoundness)
+			);
+		});
 
-    this.graph.segments.forEach((segment) => segment.draw(ctx, { color: 'white', width: 4, dash: [10, 10] }));
+		this.roadBorders = Polygon.union(
+			this.envelopes.map((envelope) => envelope.poly)
+		);
 
-    this.roadBorders.forEach((segment) => segment.draw(ctx, { color: 'white', width: 4 }));
-  }
+		this.buildings = this.#generateBuildings();
+	}
+
+	#generateBuildings() {
+		const tmpEnvelopes = [];
+		for (const seg of this.graph.segments) {
+			tmpEnvelopes.push(
+				new Envelope(
+					seg,
+					this.roadWidth + this.buildingWidth + this.spacing * 2,
+					this.roadRoundness
+				)
+			);
+		}
+
+		const guides = Polygon.union(tmpEnvelopes.map((envelope) => envelope.poly));
+
+		for (let i = 0; i < guides.length; i++) {
+			const seg = guides[i];
+			if (seg.length() < this.buildingMinLength) {
+				guides.splice(i, 1);
+				i--;
+			}
+		}
+
+		const supports = [];
+		for (const segment of guides) {
+			const len = segment.length() + this.spacing;
+			const buildingCount = Math.floor(
+				len / (this.buildingMinLength + this.spacing)
+			);
+			const buildingLength = len / buildingCount - this.spacing;
+
+			const dir = segment.directionVector();
+
+			let q1 = segment.p1;
+			let q2 = add(q1, scale(dir, buildingLength));
+			supports.push(new Segment(q1, q2));
+
+			for (let i = 2; i <= buildingCount; i++) {
+				q1 = add(q2, scale(dir, this.spacing));
+				q2 = add(q1, scale(dir, buildingLength));
+				supports.push(new Segment(q1, q2));
+			}
+		}
+
+		const bases = [];
+		for (const segment of supports) {
+			bases.push(new Envelope(segment, this.buildingWidth).poly);
+		}
+
+		const eps = 0.001;
+		for (let i = 0; i < bases.length - 1; i++) {
+			for (let j = i + 1; j < bases.length; j++) {
+				if (
+					bases[i].intersectsPoly(bases[j]) ||
+					bases[i].distanceToPoly(bases[j]) < this.spacing - eps
+				) {
+					bases.splice(j, 1);
+					j--;
+				}
+			}
+		}
+
+		// return bases;
+
+		return bases;
+	}
+
+	draw(ctx) {
+		this.envelopes.forEach((envelope) =>
+			envelope.draw(ctx, {fill: '#BBB', stroke: '#BBB', lineWidth: 15})
+		);
+
+		this.graph.segments.forEach((segment) =>
+			segment.draw(ctx, {color: 'white', width: 4, dash: [10, 10]})
+		);
+
+		this.roadBorders.forEach((segment) =>
+			segment.draw(ctx, {color: 'white', width: 4})
+		);
+
+		this.buildings.forEach((bldg) => bldg.draw(ctx));
+	}
 }
