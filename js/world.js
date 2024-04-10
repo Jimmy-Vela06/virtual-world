05c4cf028111d73ee1a709b88c95fe0d9010df38
Jimmy Vela <jimmyvela06@.com>
2024-04-10 03:11:37 -0500

    building 3d effect

--- js/world.js
+++ js/world.js
@@ -49,7 +49,7 @@ class World {
 	#generateTrees() {
 		const points = [
 			...this.roadBorders.map((s) => [s.p1, s.p2]).flat(),
-			...this.buildings.map((b) => b.points).flat(),
+			...this.buildings.map((b) => b.base.points).flat(),
 		];
 		const left = Math.min(...points.map((p) => p.x));
 		const right = Math.max(...points.map((p) => p.x));
@@ -57,7 +57,7 @@ class World {
 		const bottom = Math.max(...points.map((p) => p.y));
 
 		const illegalPolys = [
-			...this.buildings,
+			...this.buildings.map((b) => b.base),
 			...this.envelopes.map((e) => e.poly),
 		];
 
@@ -175,10 +175,10 @@ class World {
 
 		// return bases;
 
-		return bases;
+		return bases.map((b) => new Building(b));
 	}
 
-	draw(ctx, viewport) {
+	draw(ctx, viewPoint) {
 		this.envelopes.forEach((envelope) =>
 			envelope.draw(ctx, {fill: '#BBB', stroke: '#BBB', lineWidth: 15})
 		);
@@ -191,10 +191,13 @@ class World {
 			segment.draw(ctx, {color: 'white', width: 4})
 		);
 
-		for (const tree of this.trees) {
-			tree.draw(ctx, viewport);
+		const items = [...this.buildings, ...this.trees];
+		items.sort(
+			(a, b) =>
+				b.base.distanceToPoint(viewPoint) - a.base.distanceToPoint(viewPoint)
+		);
+		for (const item of items) {
+			item.draw(ctx, viewPoint);
 		}
-
-		this.buildings.forEach((bldg) => bldg.draw(ctx));
 	}
 }
